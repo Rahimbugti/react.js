5027f368bce3881c506e3c13dbeffe91a0e51ff3
Rahimbugti <162613347+Rahimbugti@users.noreply.github.com>
2025-07-22 00:26:31 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+import './index.css'
+import Navigation from './components/Navigation';
+import HeroSelecton from './components/HeroSelecton';
+
+
+ const App = () => {
+  return (
+    <div>
+    <Navigation/>
+    <HeroSelecton/>
+    </div>
+     
+  )
+}
+export default App
